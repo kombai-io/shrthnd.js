82319c19d5aeb930116630f53bb28959527df5b4
Rishi Sanghvi <rishisanghvi@Rishis-MacBook-Pro.local>
2022-12-03 16:00:31 +0530

    init

--- .history/index_20221202173015.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('./lib/engine');
-
-var css = require('./lib/engine');
-
-console.log(css(''))
--- .history/index_20221202175102.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('./lib/engine');
-
-var css = require('./lib/engine');
-var shorthands = require('./properties');
-
-console.log(css(''))
